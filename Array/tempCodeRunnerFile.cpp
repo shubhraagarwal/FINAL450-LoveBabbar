81baf91842f1913e00da26b0f78c33800f277a75
shubhraagarwal <agarwalshubhra2002@gmail.com>
2021-05-25 19:51:56 +0530

    Changed folders

--- Array/tempCodeRunnerFile.cpp
+++ Array/tempCodeRunnerFile.cpp
@@ -1,3 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
 {
-        cout << str[i];
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
+    int j = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < 0)
+        {
+            if (i != j)
+            {
+
+                swap(arr[i], arr[j]);
+                j++;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[n] << " ";
+    }
+}
