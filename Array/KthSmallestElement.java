50e8c93561237e9ea911d46203f9bb5197d75772
shubhraagarwal <agarwalshubhra2002@gmail.com>
2021-09-11 22:24:24 +0530

    modified

--- Array/KthSmallestElement.java
+++ Array/KthSmallestElement.java
@@ -5,10 +5,10 @@ public class KthSmallestElement {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        int k = sc.nextInt();
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+        int k = sc.nextInt();
         //Sorting the array
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
